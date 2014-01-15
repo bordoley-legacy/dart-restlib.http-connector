a37fce12bc70afa63be174798891dbcc0ba62d60
David Bordoley <bordoley@gmail.com>
2014-01-14 19:04:13 -0800

    suppress warnings by implementing noSuchMethod

--- test/mocks.dart
+++ test/mocks.dart
@@ -7,12 +7,39 @@ import "package:unittest/mock.dart";
 import "package:restlib_server/io.dart";
 import "package:restlib_server/server.dart";
 
-class MockHttpConnectionInfo extends Mock implements HttpConnectionInfo {}
-class MockHttpHeaders extends Mock implements HttpHeaders {}
-class MockHttpRequest extends Mock implements HttpRequest {}
-class MockHttpResponse extends Mock implements HttpResponse {}
-class MockApplication extends Mock implements Application {}
-class MockIOResource extends Mock implements IOResource {}
-class MockRoute extends Mock implements Route {}
+class MockHttpConnectionInfo extends Mock implements HttpConnectionInfo {
+  dynamic noSuchMethod(Invocation invocation) =>
+      super.noSuchMethod(invocation);
+}
+
+class MockHttpHeaders extends Mock implements HttpHeaders {
+  dynamic noSuchMethod(Invocation invocation) =>
+      super.noSuchMethod(invocation);
+}
+
+class MockHttpRequest extends Mock implements HttpRequest {
+  dynamic noSuchMethod(Invocation invocation) =>
+      super.noSuchMethod(invocation);
+}
+
+class MockHttpResponse extends Mock implements HttpResponse {
+  dynamic noSuchMethod(Invocation invocation) =>
+      super.noSuchMethod(invocation);
+}
+
+class MockApplication extends Mock implements Application {
+  dynamic noSuchMethod(Invocation invocation) =>
+      super.noSuchMethod(invocation);
+}
+
+class MockIOResource extends Mock implements IOResource {
+  dynamic noSuchMethod(Invocation invocation) =>
+      super.noSuchMethod(invocation);
+}
+
+class MockRoute extends Mock implements Route {
+  dynamic noSuchMethod(Invocation invocation) =>
+      super.noSuchMethod(invocation);
+}
 
 
